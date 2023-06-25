39220c3a8452152b91e235e59d32fdf3b53ace1d
Chuchumbrik <mussha2010@yandex.ru>
2023-06-25 19:25:41 +0300

    Sprint_6 Проектная работа + jacoco report

---
+++ src/test/java/lion/LionParametrizedTest.java
@@ -0,0 +1,38 @@
+package lion;
+
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class LionParametrizedTest {
+    private final String SEX;
+    private final boolean EXPECTED;
+
+    @Mock
+    Feline feline;
+
+    public LionParametrizedTest(String sex, boolean expected) {
+        this.SEX = sex;
+        this.EXPECTED = expected;
+    }
+
+    @Parameterized.Parameters(name = "Пол Льва.Тестовые данные:{0},{1}")
+    public static Object[][] getLionData() {
+        return new Object[][]{
+                {"Самец", true},
+                {"Самка", false}
+        };
+    }
+
+    @Test
+    public void doesHaveManeTest() throws Exception {
+        Lion lion = new Lion(SEX, feline);
+        assertEquals(EXPECTED, lion.doesHaveMane());
+    }
+}
